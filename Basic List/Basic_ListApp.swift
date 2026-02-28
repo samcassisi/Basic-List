d4c7bd45566b15c2c1e7dbeb5419133a8cdb9e60
Sam Cassisi <sam.cassisi@gmail.com>
2026-02-28 14:59:53 +1100

    Initial Commit

---
+++ Basic List/Basic_ListApp.swift
@@ -0,0 +1,17 @@
+//
+//  Basic_ListApp.swift
+//  Basic List
+//
+//  Created by Sam Cassisi on 28/2/2026.
+//
+
+import SwiftUI
+
+@main
+struct Basic_ListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Basic List/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Basic List
+//
+//  Created by Sam Cassisi on 28/2/2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
